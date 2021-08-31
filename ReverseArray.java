2b39639a38de4400c8b9ef49034cb982e9812eae
Uziel Gaeta <ugaeta@cpp.edu>
2021-08-30 23:44:35 -0700

    optimized the division by 2 to moving one bit to the right making the program run better.

--- ReverseArray.java
+++ ReverseArray.java
@@ -3,7 +3,7 @@ public class ReverseArray {
         int[] array = {1,2,3,4,5,6,7,8,9};
         int store;
         int j;
-        for(int i = 0; i < array.length/2; i++){
+        for(int i = 0; i < array.length>>1; i++){
             j = 1 + i;
             store = array[i]; 
             array[i] = array[array.length -j];
